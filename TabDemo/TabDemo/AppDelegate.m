f9e71c086a6680dd92b520c6f76da7f338d9a9c8
dogold <ligr1991@gmail.com>
2018-05-29 22:23:15 +0800

    Add log

--- TabDemo/TabDemo/AppDelegate.m
+++ TabDemo/TabDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"%s", "didFinishLaunchingWithOptions, Inactive -> Active");
     return YES;
 }
 
