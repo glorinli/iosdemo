352a5ef63f27dda7943e84fc74c5474562d45647
dogold <ligr1991@gmail.com>
2018-06-02 23:32:15 +0800

    Add cmd demo

---
+++ CmdDemo/CmdDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CmdDemo
+//
+//  Created by Glorin Li on 2018/5/31.
+//  Copyright © 2018 Glorin Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
